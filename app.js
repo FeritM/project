640a1e9feb8c27fe808ece1007f101318ce37240
Ferit <thegreetcoder@gmail.com>
2024-01-15 06:54:43 +0300

    js dersleri

---
+++ app.js
@@ -0,0 +1,26 @@
+//  console.log("Ferit Mutlu");
+//   // Document write çıktı verir          
+//   document.writeln("Hello wolrd <br>" );
+//   document.writeln("Ferit Mutlu <br>")
+
+
+//   console.log("Hello World");
+
+
+
+// //Alert ile çıktı vermek
+
+// console.log(document.location.host);
+
+// // alert("merhaba");
+// alert("f5 tuşuna lütfen basmayınız.")
+
+// console.log(window);
+// console.log(window.location.port);
+
+var degiskenİsmi =10;
+console.log(degiskenİsmi);
+
+
+
+
